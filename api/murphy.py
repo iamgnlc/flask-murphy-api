3cb856209c9aaffbb525b49132b27838d3997a95
iamgnlc <gianluca.agnocchetti@gmail.com>
2023-08-04 12:19:40 +0100

    validate number

--- api/murphy.py
+++ api/murphy.py
@@ -5,16 +5,24 @@ import random
 
 app = Flask(__name__)
 
-f = open('data.json')
-data = json.load(f)
+file = open('data.json')
+data = json.load(file)
+
+def validate(number):
+    number = int(number)
+
+    if number > 10:
+        number = 10
+
+    if number < 1:
+        number = 1
+
+    return number
 
 @app.route("/")
 @app.route("/<number>")
 def main(number = 1):
-    number = int(number)
-
-    if number > 10 or number < 0:
-        number = 1
+    number = validate(number)
 
     law = random.sample(data, number)
     return law

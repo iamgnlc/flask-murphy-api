d2eb7587b3e80283f3fa964616c4ab83fff97ce9
iamgnlc <gianluca.agnocchetti@gmail.com>
2023-08-08 16:24:17 +0100

    remove favicon

--- murphy.py
+++ murphy.py
@@ -30,11 +30,6 @@ def set_response(laws):
 
     return response
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
 @app.route("/")
 @app.route("/<number>")
 def main(number = 1):

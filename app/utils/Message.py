9e3b9b0efbe0269740f1da8277eba5231ceac856
iamgnlc <gianluca.agnocchetti@gmail.com>
2024-01-26 14:01:47 +0000

    message

--- app/utils/Message.py
+++ app/utils/Message.py
@@ -2,17 +2,17 @@ class Message:
     def success(self):
         self.code = 200
         self.status = "success"
-        return self.__error_message()
+        return self.__send_message()
 
     def not_found(self):
         self.code = 404
         self.status = "not found"
-        return self.__error_message()
+        return self.__send_message()
 
     def not_authorized(self):
         self.code = 403
         self.status = "not authorized"
-        return self.__error_message()
+        return self.__send_message()
 
-    def __error_message(self):
+    def __send_message(self):
         return {"code": self.code, "status": self.status}

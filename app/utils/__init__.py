e68d46ecf0db56dd41ffe84450c459d4ffc78ed0
iamgnlc <gianluca.agnocchetti@gmail.com>
2024-02-10 01:03:19 +0000

    remove doubvle import

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,5 +1,4 @@
 from app.utils.load_data import load_data
-from app.utils.load_data import load_data
 from app.utils.print_logo import print_logo
 from app.utils.validate import validate
 from app.utils.default_headers import default_headers
